e91d8c9471acbe79de82c73a09538db8ad88e995
kosangkeun <72964683+kosangkeun@users.noreply.github.com>
2022-05-16 19:04:25 +0900

    불필요한 요소 제거
    ㅇ

--- site/src/App.js
+++ site/src/App.js
@@ -90,20 +90,7 @@ function App() {
                         <a class="nav-link page-scroll" href="#accordion-1">Result</a>
                     </li>
                 </ul>
-                <span class="nav-item social-icons">
-                    <span class="fa-stack">
-                        <a href="#your-link">
-                            <i class="fas fa-circle fa-stack-2x"></i>
-                            <i class="fab fa-facebook-f fa-stack-1x"></i>
-                        </a>
-                    </span>
-                    <span class="fa-stack">
-                        <a href="#your-link">
-                            <i class="fas fa-circle fa-stack-2x"></i>
-                            <i class="fab fa-twitter fa-stack-1x"></i>
-                        </a>
-                    </span>
-                </span>
+                
             </div> 
         </div> 
     </nav> 
@@ -122,7 +109,6 @@ function App() {
                         <p class="introduce"> 예를 들어서 할리우드 영화 등을 보면, 원본 언어는 영어이고, 배우들 또한 영어로 말하지만, 이를 한국어로 더빙하게 되면 배우들의 입모양이 맞지 않는다는것은 당연할 것입니다</p>
                         <p class="introduce"> Talking Face 분야는, 사람이 말하는 목소리와 인물의 정면 얼굴 사진이 들어갔을때, 해당 사진속의 인물이 동시에 입력된 목소리로 말하는 듯하게 동영상으로 만들어주는 테스크 입니다</p>
                         <a class="btn-solid-lg page-scroll" href="#about">Start</a>
-                        <a class="btn-outline-lg page-scroll" href="#contact"><i class="fas fa-user"></i>Contact Me</a>
                     </div> 
                 </div> 
             </div> 
@@ -319,41 +305,7 @@ function App() {
 
 
     
-    <div id="contact" class="form-1 bg-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h2>Contact details</h2>
-                    <p class="p-heading">For any type of online project please don't hesitate to get in touch with me. The fastest way is to send me your message using the following email <a class="blue no-line" href="#your-link">contact@domain.com</a></p>
-                </div> 
-            </div> 
-            <div class="row">
-                <div class="col-lg-12">
-                    
-                 
-                    <form id="contactForm">
-                        <div class="form-group">
-                            <input type="text" class="form-control-input" id="cname" required/>
-                            <label class="label-control" for="cname">Name</label>
-                        </div>
-                        <div class="form-group">
-                            <input type="email" class="form-control-input" id="cemail" required/>
-                            <label class="label-control" for="cemail">Email</label>
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-control-textarea" id="cmessage" required></textarea>
-                            <label class="label-control" for="cmessage">Project details</label>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="form-control-submit-button">Submit</button>
-                        </div>
-                    </form>
-                  
-
-                </div>
-            </div> 
-        </div> 
-    </div> 
+    
 
 
 
@@ -362,38 +314,7 @@ function App() {
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="social-container">
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-facebook-f fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-twitter fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-pinterest-p fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-instagram fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-youtube fa-stack-1x"></i>
-                            </a>
-                        </span>
-                    </div> 
+                   
                 </div> 
             </div> 
         </div> 
