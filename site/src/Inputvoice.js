a3ea82f4f0b06d2afaf151bdb39b5a8a9aef664a
dongju923 <worhkdwkd3@gmail.com>
2022-05-27 00:12:48 +0900

    axios

--- site/src/Inputvoice.js
+++ site/src/Inputvoice.js
@@ -1,7 +1,50 @@
-import React from "react";
+import { useState } from 'react';
 import AudioRecord from "./AudioRecord";
+import axios from 'axios';
+
 
 function Inputvoice(){
+  const [audio, setAudio] = useState({audio_file:"",});
+  const onChangeImg = (e) => {
+    e.preventDefault();
+    const fileReader = new FileReader();
+    if(e.target.files[0]){
+    fileReader.readAsDataURL(e.target.files[0])}
+    fileReader.onload = () => {
+      setAudio(
+        {
+          audio_file: e.target.files[0],
+          
+        }
+      )
+      
+     
+  
+  }
+  
+};
+  
+
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(audio.audio_file)
+    if(setAudio){
+      
+      const formData = new FormData()
+      formData.append('files',audio.audio_file)
+      
+      await axios({
+        method: 'post',
+        url: '192.168.154.43:5000',
+        data: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+    }
+  }
+  
     return(
         <div id="cards-1">
         <div class="container">
@@ -14,17 +57,17 @@ function Inputvoice(){
                 <div class="col-lg-12">                   
                                                          
 
-                <form>
+                <form onSubmit={onSubmit}>
                     <div class="card">
                         <div class="card-body">
 
-                        <input type="file" id="avatar" name="avatar" accept="audio/*"></input>
-                        
+                        <input type="file" id="avatar" name="avatar" accept="audio/*" onChange={onChangeImg}></input>
+                        <button type="submit" class="form-control-submit-button"  value="Upload File"  >전송</button>
                         </div>
                     </div>
 
                     <div className="form-group">
-                        <AudioRecord></AudioRecord>
+                        
                     </div>
                 
                 </form>
