ae755b16c5019b3ae4c3a6ec541d9b33177dd11f
Glisten5325 <90368397+Glisten5325@users.noreply.github.com>
2022-06-03 20:28:42 +0900

    xaios post 받기
    아직 미완

--- site/src/Resultimg.js
+++ site/src/Resultimg.js
@@ -1,31 +1,52 @@
-import React from "react";
+import React, { useState} from "react";
+import axios from 'axios';
 
 function Resultimg(){
+
+    const [data, setData] = useState(null);
+    const onClick = async () => {
+        try {
+            const response = await axios.post(
+                'http://192.168.154.43:5000',
+            );
+            setData(response.data);
+        } catch (e) {
+          console.log(e);
+        }
+    };
+
     return(
-        <div id="accordion-1">
+    <div id="accordion-1">
         <div class="container">
+
             <div class="row">
                 <div class="col-lg-12">
-                    <h2 class="h2-heading">Result</h2>
-                    
+                    <h2 class="h2-heading">Result</h2>    
                 </div> 
             </div> 
+
             <div class="row">
                 <div class="col-lg-12">
-
                     <div class="accordion" id="accordionExample">
                         <div class="card">
-                            <img src='images/Result.jpg' width="100%" height="100%"/>
-                            
-                        </div>
-                        
-                        
-                        
-                       
-                    </div> 
+                            <div>
+                                <div>
+                                    <button onClick={onClick}>불러오기</button>
+                                </div>
 
+                                {data && (
+                                    <textarea
+                                        rows={7}
+                                        value={JSON.stringify(data, null, 2)}
+                                        readOnly={true}
+                />
+                                )}
+                            </div>
+                        </div>
+                    </div> 
                 </div> 
             </div> 
+
         </div> 
     </div> 
     )
